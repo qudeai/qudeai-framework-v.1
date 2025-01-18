72a123891d837ff320711a2c880f6cb6d7410f16
Qude-ai agent framework <official@qude.ai>
2025-01-18 06:42:44 +0530

    defineagent.ts

--- src/defineagent.ts
+++ src/defineagent.ts
@@ -1,10 +1,11 @@
 export const agentDetails = {
-    name: "test",
+    name: "Test",
     symbol: "test",
-    initialBuyAmount: 0,
+    initialBuyAmount: 0.05,
     description: "My first AI agent token on Pumpfun",
     twitter: "https://twitter.com/example",
     telegram: "https://t.me/example",
     website: "https://example.com",
     imagePath: "src/agent_logo/example.png",
+    personality: "Andrew tate",
 };
